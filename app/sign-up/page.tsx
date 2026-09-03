b2bc43f21457429b9663b4a85b51b0d26c488402
Aayushtamang <jimba.lama22@gmail.com>
2026-09-03 19:40:06 +0545

    authentication setup

--- app/sign-up/page.tsx
+++ app/sign-up/page.tsx
@@ -1,5 +1,138 @@
+"use client";
+
+import { Button } from "@/components/ui/button";
+import {
+  Card,
+  CardContent,
+  CardDescription,
+  CardFooter,
+  CardHeader,
+  CardTitle,
+} from "@/components/ui/card";
+import { signUp } from "@/lib/auth/auth-client";
+import { Input } from "@/components/ui/input";
+import { Label } from "@/components/ui/label";
+import Link from "next/link";
+import { useState } from "react";
+import { useRouter } from "next/navigation";
+
 export default function SignUp() {
-    return (
-        <div>SignUp Page</div>
-    );
+  const [name, setName] = useState("");
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+
+  const [error, setError] = useState("");
+  const [loading, setLoading] = useState(false);
+
+  const router = useRouter();
+
+  async function handleSubmit(e: React.FormEvent) {
+    e.preventDefault();
+
+    setError("");
+    setLoading(true);
+
+    try {
+      const result = await signUp.email({
+        name,
+        email,
+        password,
+      });
+
+      if (result.error) {
+        setError(result.error.message ?? "Failed to sign up");
+      } else {
+        router.push("/dashboard");
+      }
+    } catch (error) {
+      console.log(error);
+      setError("An unexpected errror occured");
+    } finally {
+      setLoading(false);
+    }
+  }
+  return (
+    <div className="flex min-h-[calc(100vh-4rem)] items-center justify-center bg-white p-4">
+      <Card className="w-full max-w-md border-gray-200 shadow-lg">
+        <CardHeader className="space-y-1">
+          <CardTitle className="text-2xl font-bold text-black">
+            Sign Up
+          </CardTitle>
+          <CardDescription className="text-gray-600">
+            Create an account to start tracking your job application
+          </CardDescription>
+        </CardHeader>
+        <form onSubmit={handleSubmit} action="" className="space-y-4">
+          <CardContent className="space-y-4">
+            {error && (
+              <div className="rounded-md bg-destructive/15 p-3 text-sm text-destructive">
+                {error}
+              </div>
+            )}
+            <div className="space-y-2">
+              <Label htmlFor="name" className="text-gray-700">
+                Name
+              </Label>
+              <Input
+                id="name"
+                type="text"
+                placeholder="John Doe"
+                required
+                className="border-gray-300 focus:border-primary focus:ring-primary"
+                value={name}
+                onChange={(e) => setName(e.target.value)}
+              />
+            </div>
+            <div className="space-y-2">
+              <Label htmlFor="email" className="space-y-2">
+                Email
+              </Label>
+              <Input
+                id="email"
+                type="email"
+                placeholder="john@gmail.com"
+                required
+                className="border-gray-300 focus:border-primary focus:ring-primary"
+                value={email}
+                onChange={(e) => setEmail(e.target.value)}
+              />
+            </div>
+            <div className="space-y-2">
+              <Label htmlFor="password" className="space-y-2">
+                Password
+              </Label>
+              <Input
+                id="password"
+                type="password"
+                placeholder="John Doe"
+                required
+                minLength={8}
+                className="border-gray-300 focus:border-primary focus:ring-primary"
+                value={password}
+                onChange={(e) => setPassword(e.target.value)}
+              />
+            </div>
+          </CardContent>
+          <CardFooter className="flex flex-col space-y-4">
+            <Button
+              type="submit"
+              className="w-full bg-primary hover:bg-primary/90"
+              disabled={loading}
+            >
+              {loading ? "Creating account..." : "Sign Up"}
+            </Button>
+            <p className="text-center text-sm text-gray-600">
+              Already have an account?{" "}
+              <Link
+                href="/sign-in"
+                className="font-medium text-primary hover:underline"
+              >
+                Sign In
+              </Link>
+            </p>
+          </CardFooter>
+        </form>
+      </Card>
+    </div>
+  );
 }
---
+++ lib/auth/auth.ts
@@ -0,0 +1,34 @@
+import { betterAuth } from "better-auth";
+import { mongodbAdapter } from "better-auth/adapters/mongodb";
+import { MongoClient } from "mongodb";
+import { redirect } from "next/dist/server/api-utils";
+import { headers } from "next/headers";
+
+const client = new MongoClient(process.env.MONGODB_URI!);
+const db = client.db();
+
+export const auth = betterAuth({
+    database: mongodbAdapter(db, {
+        client,
+    }),
+    emailAndPassword: {
+        enabled: true
+    }
+});
+
+export async function getSession() {
+    const result = await auth.api.getSession({
+        headers: await headers()
+    })
+
+    return result;
+}
+
+export async function signOut() {
+    const result = await auth.api.signOut({
+        headers: await headers()
+    });
+    if (result.success) {
+        redirect("/sign-in")
+    }
+}

b5c9ef012cb536f95fab51f238d49a519517f073
Aayushtamang <jimba.lama22@gmail.com>
2026-09-10 18:24:45 +0545

    fix mongoose issue

--- lib/auth/auth.ts
+++ lib/auth/auth.ts
@@ -1,11 +1,12 @@
 import { betterAuth } from "better-auth";
 import { mongodbAdapter } from "better-auth/adapters/mongodb";
-import { MongoClient } from "mongodb";
+import { headers } from "next/headers";
 import { redirect } from "next/navigation";
 import { initializeUserBoard } from "../init-user-board";
-import { headers } from "next/headers";
+import connectDB from "../db";
 
-const client = new MongoClient(process.env.MONGODB_URI!);
+const mongooseInstance = await connectDB();
+const client = mongooseInstance.connection.getClient();
 const db = client.db();
 
 export const auth = betterAuth({
@@ -46,6 +47,7 @@ export async function signOut() {
   const result = await auth.api.signOut({
     headers: await headers(),
   });
+
   if (result.success) {
     redirect("/sign-in");
   }
